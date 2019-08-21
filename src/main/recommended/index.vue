53ff0202aedaffa869811afabf06a529cd78fb05
E2875888382 <2875888382@qq.com>
2019-08-21 09:23:37 +0800

    i

--- src/main/recommended/index.vue
+++ src/main/recommended/index.vue
@@ -1,14 +1,111 @@
 <template>
-  
+<div>
+    <div class="col-12 top">
+        <van-icon name="arrow-left" @click="back"/><p>小说推荐</p>
+    </div>
+    <div class="col-12 class" style="background-color:whitesmoke;height:132px">
+        <span :class="{'col-2':true,'active':index == num}" v-for="(item,index) in Class" :key="index" @click="changeClass(index)">
+            {{ item }}
+        </span>
+    </div>
+    <div class="col-12">
+        <van-card v-for="(item,index) in support" :key="index">
+            <p slot="title" class="title">{{ item.name }}</p>
+            <p slot="desc" class="desc">是时势造英雄还是英雄造时势？肖恩带着技能面板穿越异界，用实力和手段成就战神领主！</p>
+            <div slot="tags" class="ifo">
+                <span class="author"><van-icon name="contact" />唐家三少</span>
+                <div>
+                    <van-tag plain>仙侠</van-tag>
+                    <van-tag plain type="primary">291.62万字</van-tag>
+                </div>
+            </div>
+            <el-image slot="thumb" style=" height:110px;width:80px" :src="item.cover" fit="fill"></el-image>
+        </van-card>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            num:0,
+            Class:[
+                '全部','玄幻','奇幻','武侠','仙侠','都市','现实','军事',
+                '历史','游戏','体育','科幻','悬疑','轻小说'
+            ],
+            support:[
+                {cover:'https://bookcover.yuewen.com/qdbimg/349573/1015929450/90',name:'我的眼睛能鉴宝'},
+                {cover:'https://bookcover.yuewen.com/qdbimg/349573/1015859383/90',name:'我是篮坛巨星'},
+                {cover:'https://bookcover.yuewen.com/qdbimg/349573/1015839132/90',name:'反派驾临'},
+                {cover:'https://bookcover.yuewen.com/qdbimg/349573/1013744668/90',name:'谍战最新力作'},
+                {cover:'https://bookcover.yuewen.com/qdbimg/349573/1015717336/90',name:'最强赏金猎人'},
+                {cover:'https://bookcover.yuewen.com/qdbimg/349573/1015407320/90',name:'大明最后一个狠人'},
+            ]
+        }
+    },
+    methods:{
+        back(){
+            this.$router.push({path:'/'});
+        },
+        changeClass(index){
+            this.num = index;
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+.active{
+    border:1px solid red;
+    color:red;
+}
+.top{
+    display: flex;
+    align-items:center;
+    margin:10px 0 ;
+}
+.top p{
+    margin:0;
+    padding-left:20px;
+}
+.van-card{
+    background-color: #fff;
+    border-bottom: 1px solid #f5f7fa;
+    height:125px;
+}
+.ifo{
+    display:flex;
+    justify-content: space-between;
+}
+.title{
+    font-weight:600;
+    font-size:14px;
+    margin-bottom:8px;
+}
+.desc{
+    color:#999;
+    margin-bottom: 8px;
+    font-size:12px;
+}
+.author{
+    color:#999;
+    font-size:12px;
+    display:flex;
+    align-items:center;
+}
+.el-image{
+    box-shadow: 0 1px 6px rgba(0,0,0,.35), 0 0 5px #f9f2e9 inset;
+}
+.class{
+    display: flex;
+    flex-wrap: wrap;
+}
+.class span{
+    margin: 10px 5px;
+    padding:0;
+    border-radius: 30px;
+    text-align:center;
+}
 </style>
 
